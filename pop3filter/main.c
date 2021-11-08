6a902a2f49706e6240d9008d21594636688ad2e9
GastonDeS <65664359+GastonDeS@users.noreply.github.com>
2021-11-08 13:17:12 -0300

    add originAddr

--- pop3filter/main.c
+++ pop3filter/main.c
@@ -18,6 +18,7 @@ extern struct proxy_args args;
 
 static address_info proxy_addr;
 static address_info admin_proxy_addr;
+static address_info origin_addr_data;
 
 //Sockets tienen que ser gloabales
 static int proxy = -1;
@@ -43,8 +44,7 @@ static unsigned short port(const char *s) {
     char *end = 0;
     const long sl = strtol(s, &end, 10);
 
-    if (end == s || '\0' != *end || ((LONG_MIN == sl || LONG_MAX == sl) && ERANGE == errno) || sl < 0 || sl > USHRT_MAX)
-    {
+    if (end == s || '\0' != *end || ((LONG_MIN == sl || LONG_MAX == sl) && ERANGE == errno) || sl < 0 || sl > USHRT_MAX){
         fprintf(stderr, "port should in in the range of 1-65536: %s\n", s);
         exit(1);
     }
@@ -79,58 +79,56 @@ void parse_args(const int argc, const char **argv) {
 
     admin_proxy_addr.port = 9090;
     proxy_addr.port = 1110;
+    origin_addr_data.port = 110;
 
     int option_arg;
-    while (true)
-    {
+    while (true) {
         option_arg = getopt(argc, (char *const *)argv, "e:hl:L:o:p:P:t:v");
 
         if (option_arg == -1)
             break;
 
-        switch (option_arg)
-        {
-        case 'e':
-            args.stderr_file_path = optarg;
-            break;
-        case 'h':
-            usage(argv[0]);
-            break;
-        case 'l':
-            args.listen_pop3_address = optarg;
-            break;
-        case 'L':
-            args.listen_pop3_admin_address = optarg;
-            break;
-        case 'o':
-            admin_proxy_addr.port = port(optarg);
-            break;
-        case 'p':
-            proxy_addr.port = port(optarg);
-            break;
-        case 'P':
-            //Setear puerto origin
-            break;
-        case 't':
-            break;
-        case 'v':
-            version();
-            exit(0);
-        default:
-            fprintf(stderr, "unknown argument %d.\n", option_arg);
-            exit(1);
+        switch (option_arg) {
+            case 'e':
+                args.stderr_file_path = optarg;
+                break;
+            case 'h':
+                usage(argv[0]);
+                break;
+            case 'l':
+                args.listen_pop3_address = optarg;
+                break;
+            case 'L':
+                args.listen_pop3_admin_address = optarg;
+                break;
+            case 'o':
+                admin_proxy_addr.port = port(optarg);
+                break;
+            case 'p':
+                proxy_addr.port = port(optarg);
+                break;
+            case 'P':
+                origin_addr_data.port = port(optarg);
+                break;
+            case 't':
+                break;
+            case 'v':
+                version();
+                exit(0);
+            default:
+                fprintf(stderr, "unknown argument %d.\n", option_arg);
+                exit(1);
         }
     }
-    if (optind < argc)
-    {
+    if (argc - optind != 1) {
         fprintf(stderr, "argument not accepted: ");
-        while (optind < argc)
-        {
+        while (optind < argc) {
             fprintf(stderr, "%s ", argv[optind++]);
         }
         fprintf(stderr, "\n");
         exit(1);
     }
+    args.listen_origin_address = (char *)argv[optind];
 }
 
 int main(int argc, char const **argv) {
@@ -174,9 +172,13 @@ int main(int argc, char const **argv) {
         exit(1);
     }
 
+    printf("FAILS HERE BINDS\n");
+
+
     if((ans = bind(proxy, (struct sockaddr *)&proxy_addr.addr.storage, proxy_addr.addr_len)) < 0) {
         err_msg = "bind() failed for proxy";
         close(proxy);
+        printf("%d",errno);
         exit(1);
     }
 
@@ -186,16 +188,20 @@ int main(int argc, char const **argv) {
         exit(1);
     }
 
-    if ((ans = listen(proxy, BACKLOG)) < 0)
-    {
+    printf("FAILS HERE LISTEN\n");
+
+
+    //FAIL HERE
+    if ((ans = listen(proxy, BACKLOG)) < 0) {
         err_msg = "listen() failed for proxy";
+        printf("%s %d\n",err_msg,errno);
         close(proxy);
         exit(1);
     }
 
-    if ((ans = listen(admin_proxy, BACKLOG)) < 0)
-    {
+    if ((ans = listen(admin_proxy, BACKLOG)) < 0) {
         err_msg = "listen() failed for proxy admin";
+        printf("%s %d\n",err_msg,errno);
         close(admin_proxy);
         exit(1);
     }
@@ -206,8 +212,7 @@ int main(int argc, char const **argv) {
     signal(SIGTERM, sigterm_handler); //kill
     signal(SIGINT, sigterm_handler); //ctrl c
 
-    if (selector_fd_set_nio(proxy) == -1)
-    {
+    if (selector_fd_set_nio(proxy) == -1) {
         err_msg = "getting server proxy flags";
         goto finally;
     }
@@ -226,8 +231,7 @@ int main(int argc, char const **argv) {
     }
 
     selector = selector_new(1024); 
-    if (selector == NULL)
-    {
+    if (selector == NULL) {
         err_msg = "unable to create selector";
         goto finally;
     }
@@ -244,49 +248,43 @@ int main(int argc, char const **argv) {
         .handle_close = NULL, // nada que liberar
     };
 
+    set_address(&origin_addr_data,args.listen_origin_address);
+
     //Agregar origin addr al selector register del proxy
-    if ((ss = selector_register(selector, proxy, &pop3, OP_READ, NULL)) != SELECTOR_SUCCESS)
-    {
+    if ((ss = selector_register(selector, proxy, &pop3, OP_READ, &origin_addr_data)) != SELECTOR_SUCCESS) {
         err_msg = "registering fd for proxy";
         goto finally;
     }
 
-    if ((ss = selector_register(selector, admin_proxy, &pop3_admin, OP_READ, NULL)) != SELECTOR_SUCCESS)
-    {
+    if ((ss = selector_register(selector, admin_proxy, &pop3_admin, OP_READ, NULL)) != SELECTOR_SUCCESS) {
         err_msg = "registering fd for proxy admin";
         goto finally;
     }
 
-    for (; !done;)
-    {
+    for (; !done;) {
         err_msg = NULL;
         ss = selector_select(selector);
-        if (ss != SELECTOR_SUCCESS)
-        {
+        if (ss != SELECTOR_SUCCESS) {
             err_msg = "serving";
             goto finally;
         }
     }
-    if (err_msg == NULL)
-    {
+    if (err_msg == NULL) {
         err_msg = "closing";
     }
 
     int ret = 0;
 
 finally:
-    if (ss != SELECTOR_SUCCESS)
-    {
+    if (ss != SELECTOR_SUCCESS) {
         fprintf(stderr, "%s: %s\n", (err_msg == NULL) ? "" : err_msg, ss == SELECTOR_IO ? strerror(errno) : selector_error(ss));
         ret = 2;
     }
-    else if (err_msg)
-    {
+    else if (err_msg) {
         perror(err_msg);
         ret = 1;
     }
-    if (selector != NULL)
-    {
+    if (selector != NULL) {
         selector_destroy(selector);
     }
     selector_close();
